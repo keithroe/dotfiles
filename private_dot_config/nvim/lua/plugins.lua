9ab7dbff486c1bb1c59bf7ebbcff4470524c03ef
keith <keithroe@gmail.com>
2023-01-23 08:00:40 -0700

    New neovim config first checkin

--- private_dot_config/nvim/lua/plugins.lua
+++ private_dot_config/nvim/lua/plugins.lua
@@ -1,12 +1,57 @@
-return require('packer').startup(function(use)
 
+-- ensure the packer plugin manager is installed
+local ensure_packer = function()
+    local fn = vim.fn
+    local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+    if fn.empty(fn.glob(install_path)) > 0 then
+        fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+        vim.cmd([[packadd packer.nvim]])
+        return true
+    end
+    return false
+end
 
+local packer_bootstrap = ensure_packer()
 
-    use {
-        -- Packer can manage itself
-        'wbthomason/packer.nvim',
-    }
+require("packer").startup(function(use)
+    -- Packer can manage itself
+    use("wbthomason/packer.nvim")
+    -- Collection of common configurations for the Nvim LSP client
+    use("neovim/nvim-lspconfig")
+    -- Visualize lsp progress
+    use({
+        "j-hui/fidget.nvim",
+        config = function()
+            require("fidget").setup()
+        end
+    })
 
+    -- Autocompletion framework
+    use("hrsh7th/nvim-cmp")
+    use({
+        -- cmp LSP completion
+        "hrsh7th/cmp-nvim-lsp",
+        -- cmp Snippet completion
+        "hrsh7th/cmp-vsnip",
+        -- cmp Path completion
+        "hrsh7th/cmp-path",
+        "hrsh7th/cmp-buffer",
+        after = { "hrsh7th/nvim-cmp" },
+        requires = { "hrsh7th/nvim-cmp" },
+    })
+    -- See hrsh7th other plugins for more great completion sources!
+    -- Snippet engine
+    use('hrsh7th/vim-vsnip')
+    -- Adds extra functionality over rust analyzer
+    use("simrat39/rust-tools.nvim")
+
+    -- Optional
+    use("nvim-lua/popup.nvim")
+    use("nvim-lua/plenary.nvim")
+    use("nvim-telescope/telescope.nvim")
+
+    -- Some color scheme other then default
+    -- use("arcticicestudio/nord-vim")
     use ({
         -- color schemes
         'shaunsingh/nord.nvim',
@@ -14,7 +59,6 @@ return require('packer').startup(function(use)
     })
 
     use {
-
         'nvim-tree/nvim-tree.lua',
         requires = {
             'nvim-tree/nvim-web-devicons', -- optional, for file icons
@@ -22,40 +66,8 @@ return require('packer').startup(function(use)
     }
 
     use {
-        -- Used to install rust analyzer
-        "williamboman/mason.nvim",
-        "williamboman/mason-lspconfig.nvim",
-        "neovim/nvim-lspconfig",
-    }
-
-    use {
-        -- Attach rust-analyzer to nvim
-        'simrat39/rust-tools.nvim',
-    }
-
-    use {
         -- treesitter
         'nvim-treesitter/nvim-treesitter',
-    }
-
-    use {
-        -- Completion framework:
-        'hrsh7th/nvim-cmp',
-    }
-
-    use {
-        -- LSP completion source:
-        'hrsh7th/cmp-nvim-lsp',
-    }
-
-    use {
-        -- Useful completion sources:
-        'hrsh7th/cmp-nvim-lua',
-        'hrsh7th/cmp-nvim-lsp-signature-help',
-        'hrsh7th/cmp-vsnip',
-        'hrsh7th/cmp-path',
-        'hrsh7th/cmp-buffer',
-        'hrsh7th/vim-vsnip',
     }
 
     use {
@@ -74,7 +86,6 @@ return require('packer').startup(function(use)
         -- Bracket matching
         "windwp/nvim-autopairs",
     }
-
     use {
         -- Cursor jump highlighting
         "danilamihailov/beacon.nvim",
@@ -91,4 +102,62 @@ return require('packer').startup(function(use)
         requires = { {'nvim-lua/plenary.nvim'} }
     }
 
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+    }
+
 end)
+
+-- the first run will install packer and our plugins
+if packer_bootstrap then
+    require("packer").sync()
+    return
+end
+
+
+--------------------------------------------------------------------------------
+-- Plugin setup
+--------------------------------------------------------------------------------
+
+
+-- nvim-tree
+--------------------------------------------------------------------------------
+require("nvim-tree").setup({
+  sort_by = "case_sensitive",
+  view = {
+    adaptive_size = true,
+    mappings = {
+      list = {
+        { key = "u", action = "dir_up" },
+      },
+    },
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = true,
+  },
+})
+
+
+-- nvim-treesitter
+--------------------------------------------------------------------------------
+require'nvim-treesitter.configs'.setup {
+    -- A list of parser names, or "all" (the four listed parsers should always be installed)
+    ensure_installed = { "c", "diff", "lua", "vim", "help", "rust", "toml", "cpp", "python" },
+    auto_install = true,
+    highlight = {
+        enable = true,
+        additional_vim_regex_highlighting=false,
+    },
+    ident = { enable = true },
+    rainbow = {
+        enable = true,
+        extended_mode = true,
+        max_file_lines = nil,
+    }
+}
+
+
